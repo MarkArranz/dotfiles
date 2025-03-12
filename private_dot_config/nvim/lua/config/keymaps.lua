94afaee3383065db3439ebededfa4155938f9f35
Mark Arranz <mark.arranz@gmail.com>
2025-03-11 23:11:17 -0700

    Make vim-kitty-navigator work

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,26 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Ensure vim-kitty-navigator keymapping doesn't get overwritten:
+if os.getenv("TERM") == "xterm-kitty" then
+  vim.keymap.set(
+    "n",
+    "<c-h>",
+    "<cmd>KittyNavigateLeft<cr>",
+    { remap = false, silent = true, desc = "KittyNavigateLeft" }
+  )
+  vim.keymap.set(
+    "n",
+    "<c-j>",
+    "<cmd>KittyNavigateDown<cr>",
+    { remap = false, silent = true, desc = "KittyNavigateDown" }
+  )
+  vim.keymap.set("n", "<c-k>", "<cmd>KittyNavigateUp<cr>", { remap = false, silent = true, desc = "KittyNavigateUp" })
+  vim.keymap.set(
+    "n",
+    "<c-l>",
+    "<cmd>KittyNavigateRight<cr>",
+    { remap = false, silent = true, desc = "KittyNavigateRight" }
+  )
+end
