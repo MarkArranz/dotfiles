12c08fca97278b311592ea1dc62ae3c28dd2a133
Mark Arranz <mark.arranz@gmail.com>
2025-10-02 17:08:20 -0700

    [aerospace][sketchybar] Update sketchybar to use aerospace workspaces

---
+++ private_dot_config/sketchybar/items/aerospace.lua
@@ -0,0 +1,127 @@
+local icons = require("icons")
+local colors = require("colors")
+local settings = require("settings")
+local app_icons = require("helpers.app_icons")
+
+local max_workspaces = 9
+local focused_workspace_index = nil
+local is_show_windows = true
+
+local workspaces = {}
+
+-- Update workspace UI to reflect current window state
+-- Shows window icons when apps are open and hides them when emtpy,
+-- except for the focused workspace which shows a placeholder.
+local function updateWindows(workspace_index)
+	local get_windows =
+		string.format("aerospace list-windows --workspace %s --format '%%{app-name}' --json", workspace_index)
+
+	sbar.exec(get_windows, function(open_windows)
+		local icon_line = ""
+		local no_app = true
+
+		for i, open_window in ipairs(open_windows) do
+			no_app = false
+			local app = open_window["app-name"]
+			local lookup = app_icons[app]
+			-- Fallback to default icon if app-specific icon isn't found
+			local icon = ((lookup == nil) and app_icons["default"] or lookup)
+			icon_line = icon_line .. " " .. icon
+		end
+
+		sbar.animate("tanh", 10, function()
+			if no_app then
+				if workspace_index == focused_workspace_index then
+					-- Show placeholder for empty focused workspace
+					icon_line = " —"
+					workspaces[workspace_index]:set({
+						icon = { drawing = true },
+						label = { drawing = true, string = icon_line },
+						background = { drawing = true },
+						padding_right = 1,
+						padding_left = 4,
+					})
+					return
+				end
+
+				-- Hide empty unfocused workspaces
+				workspaces[workspace_index]:set({
+					icon = { drawing = false },
+					label = { drawing = false },
+					background = { drawing = false },
+					padding_right = 0,
+					padding_left = 0,
+				})
+				return
+			end
+
+			workspaces[workspace_index]:set({
+				icon = { drawing = true },
+				label = { drawing = true, string = icon_line },
+				background = { drawing = true },
+				padding_right = 1,
+				padding_left = 4,
+			})
+		end)
+	end)
+end
+
+for workspace_index = 1, max_workspaces do
+	local workspace = sbar.add("item", {
+		icon = {
+			font = { family = settings.font.numbers },
+			string = workspace_index,
+			padding_left = 15,
+			padding_right = 8,
+			color = colors.white,
+			highlight_color = colors.red,
+		},
+		label = {
+			padding_right = 20,
+			color = colors.grey,
+			highlight_color = colors.white,
+			font = "sketchybar-app-font:Regular:16.0",
+			y_offset = -1,
+		},
+		padding_right = 1,
+		padding_left = 1,
+		background = {
+			color = colors.bg1,
+			border_width = 3,
+			border_color = colors.transparent,
+		},
+	})
+
+	workspaces[workspace_index] = workspace
+
+	workspace:subscribe("aerospace_workspace_change", function(env)
+		focused_workspace_index = tonumber(env.FOCUSED_WORKSPACE)
+		local is_focused = focused_workspace_index == workspace_index
+
+		sbar.animate("tanh", 10, function()
+			workspace:set({
+				icon = { highlight = is_focused },
+				label = { highlight = is_focused },
+				background = {
+					border_color = is_focused and colors.black or colors.bg2,
+				},
+			})
+		end)
+	end)
+
+	workspace:subscribe("aerospace_focus_change", function()
+		updateWindows(workspace_index)
+	end)
+
+	-- Set initial workspace state
+	updateWindows(workspace_index)
+	sbar.exec("aerospace list-workspaces --focused", function(focused_workspace)
+		workspaces[tonumber(focused_workspace)]:set({
+			icon = { highlight = true },
+			label = { highlight = true },
+			background = {
+				border_color = colors.black,
+			},
+		})
+	end)
+end
--- private_dot_config/sketchybar/items/init.lua
+++ private_dot_config/sketchybar/items/init.lua
@@ -1,6 +1,7 @@
 require("items.apple")
 require("items.menus")
-require("items.spaces")
+require("items.aerospace")
+-- require("items.spaces")
 -- require("items.space_mode")
 require("items.calendar")
 require("items.widgets")
